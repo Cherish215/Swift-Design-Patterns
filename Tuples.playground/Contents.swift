6df42f8556228748fe8eaf37f858b2b61ccbfc3a
Cherish215 <just.forever215@gmail.com>
2018-04-08 00:43:48 +0800

    SportsStore & Tuples Playforund

---
+++ Tuples.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var myProduct = ("Kayak", "A boat for one person", "Watersports", 275.0, 10)
+
+func writeProductDetails(product: (String, String, String, Double, Int)) {
+    print("Name: \(product.0)")
+    print("Description: \(product.1)")
+    print("Category: \(product.2)")
+    let formattedPrice = NSString(format: "$%.2lf", product.3)
+    print("Price: \(formattedPrice)")
+}
+
+writeProductDetails(product: myProduct)
